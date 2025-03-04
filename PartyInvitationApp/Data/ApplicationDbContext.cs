b9d553bfc5fa93a420bdc109f99fa9fef944636a
Komalpreet <kkomalpreet462@gmail.com>
2025-03-04 18:49:42 -0500

    Added Applicationdb context

--- PartyInvitationApp/Data/ApplicationDbContext.cs
+++ PartyInvitationApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace PartyInvitationApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using PartyInvitationApp.Models;
+
+namespace PartyInvitationApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Party> Parties { get; set; }
+        public DbSet<Invitation> Invitations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Invitation>()
+                .Property(inv => inv.Status)
+                .HasConversion<string>();
+        }
     }
 }
