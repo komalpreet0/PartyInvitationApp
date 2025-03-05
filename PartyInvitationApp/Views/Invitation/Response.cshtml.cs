dd9b35de2f63e66d25a482faf8df672bfd807df8
Komalpreet <kkomalpreet462@gmail.com>
2025-03-04 19:37:36 -0500

    Updated view

--- PartyInvitationApp/Views/Invitation/Response.cshtml.cs
+++ PartyInvitationApp/Views/Invitation/Response.cshtml.cs
@@ -1,12 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PartyInvitationApp.Data;
+using PartyInvitationApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace PartyInvitationApp.Views.Invitation
+namespace PartyInvitationApp.Pages.InvitationResponses
+
 {
     public class ResponseModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public ResponseModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public PartyInvitationApp.Models.Invitation Invitation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Invitation = await _context.Invitations.FindAsync(id);
+            if (Invitation == null)
+                return NotFound();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id, string response)
+        {
+            var invitation = await _context.Invitations.FindAsync(id);
+            if (invitation == null)
+                return NotFound();
+
+            invitation.Status = response == "Yes" ? InvitationStatus.RespondedYes : InvitationStatus.RespondedNo;
+
+            _context.Update(invitation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Party/Manage", new { id = invitation.PartyId });
         }
     }
 }
