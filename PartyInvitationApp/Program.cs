a444583e0ece5e3d5f847a39fe2ce490b73c9fd6
Komalpreet <kkomalpreet462@gmail.com>
2025-03-04 19:59:33 -0500

    Updated appsetting and program.cs

--- PartyInvitationApp/Program.cs
+++ PartyInvitationApp/Program.cs
@@ -1,15 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PartyInvitationApp.Data;
+using PartyInvitationApp.Services;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+// Configure SQL Server LocalDB
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+// Add services to the container
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<EmailService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Apply Migrations Automatically for SQL Server LocalDB
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate(); // Ensure database is updated
+}
+
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,7 +40,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
